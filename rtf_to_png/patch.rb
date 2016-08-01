3f02c074d4bc97ebef5637d9952df5eeceef8153
Menno van der Graaf <tituxs@localhost.localdomain>
2016-08-01 12:30:59 +0200

    rename collored_patches to patch & implement chunky_png
    changed quite a lot regarding to the patch generation,
    including a first implementation of the colored patch

---
+++ rtf_to_png/patch.rb
@@ -0,0 +1,67 @@
+require "chunky_png"
+
+class Patch
+    module Types
+        SENTENCE_START=0
+        #SENTENCE_END=1
+        WORD_START=2
+        MID=3
+        #WORD_END=4
+    end
+
+    def set(char, color, type = MID)
+        @char = char
+        @color = color
+        @type = type
+    end
+
+    def to_img(mode, width, height = 0, offset = 0)
+        height = width * 1.6
+        height.floor
+        offset = width * 0.5
+        case mode
+        when 'patch'
+            ret = to_patch(width, height, offset, @color)
+        when 'glyph_colored_simple'
+            ret = to_glyph(width, @color)
+        else
+            puts 'to_img INVALID MODE: "' + mode.to_s + '"'
+        end
+
+        return ret
+    end
+
+private
+    def to_patch(width, height, offset, color)
+        img = ChunkyPNG::Image.new(width + offset, height, ChunkyPNG::Color::TRANSPARENT)
+
+        case @type
+        when Types::SENTENCE_START
+            top_offset = 0
+            bottom_offset = 0
+        when Types::WORD_START
+            top_offset = 0
+            bottom_offset = offset
+        when Types::MID
+            top_offset = offset
+            bottom_offset = 0
+        end
+
+        points = Array.new()
+        points[0] = ChunkyPNG::Point(top_offset, 0)
+        points[1] = ChunkyPNG::Point(60, 0)
+        points[2] = ChunkyPNG::Point(60, height)
+        points[3] = ChunkyPNG::Point(bottom_offset, height)
+        img.polygon(points, @color, @color)
+    end
+
+    def to_glyph(width, color)
+        #img = ChunkyPNG::Image.new(width + offset, height, ChunkyPNG::Color::TRANSPARENT)
+        img = ChunkyPNG::Image.new(600, 300, ChunkyPNG::Color::WHITE)
+        wr = ChunkyText::Writer.new(img,'default_font')
+        wr.size = 30
+        wr.color = ChunkyPNG::Color::BLACK
+        wr.write("*",100,150)
+        return img
+    end
+end
